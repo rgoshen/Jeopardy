041584365ac8cba7d094c216edf758f3cf5a09f3
Richard Goshen <rick.goshen@gmail.com>
2020-09-21 19:01:42 -0700

    Add files via upload

---
+++ scripts/jeopardy.js
@@ -0,0 +1,119 @@
+const NUM_CATEGORIES = 6;
+const NUM_QUESTIONS_PER_CAT = 5;
+// categories is the main data structure for the app; it looks like this:
+
+//  [
+//    { title: "Math",
+//      clues: [
+//        {question: "2+2", answer: 4, showing: null},
+//        {question: "1+1", answer: 2, showing: null}
+//        ...
+//      ],
+//    },
+//    { title: "Literature",
+//      clues: [
+//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
+//        {question: "Bell Jar Author", answer: "Plath", showing: null},
+//        ...
+//      ],
+//    },
+//    ...
+//  ]
+
+let categories = [];
+
+/** Get NUM_CATEGORIES random category from API.
+ *
+ * Returns array of category ids
+ */
+
+function getCategoryIds() {}
+
+/** Return object with data about a category:
+ *
+ *  Returns { title: "Math", clues: clue-array }
+ *
+ * Where clue-array is:
+ *   [
+ *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
+ *      {question: "Bell Jar Author", answer: "Plath", showing: null},
+ *      ...
+ *   ]
+ */
+
+function getCategory(catId) {}
+
+/** Fill the HTML table#jeopardy with the categories & cells for questions.
+ *
+ * - The <thead> should be filled w/a <tr>, and a <td> for each category
+ * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
+ *   each with a question for each category in a <td>
+ *   (initally, just show a "?" where the question/answer would go.)
+ */
+
+async function fillTable() {}
+
+function renderBoard() {
+  $("body").append($("<table>").attr("id", "jeopardy"));
+
+  $("#jeopardy").append($("<thead>"));
+  $("#jeopardy").append($("<tbody>"));
+
+  // add category header row
+  let $tr = $("<tr>");
+  for (let catIndex = 0; catIndex < NUM_CATEGORIES; catIndex++) {
+    $tr.append($("<th>").text("category"));
+  }
+  $("#jeopardy thead").append($tr);
+
+  // add rows for clues
+  for (let clueIndex = 0; clueIndex < NUM_QUESTIONS_PER_CAT; clueIndex++) {
+    let $tr = $("<tr>");
+    for (let catIndex = 0; catIndex < NUM_CATEGORIES; catIndex++) {
+      $tr.append(
+        $("<td>")
+          .attr("id", `${clueIndex}-${catIndex}`)
+          .text("?")
+          .addClass("hover initial")
+      );
+    }
+    $("#jeopardy tbody").append($tr);
+  }
+}
+
+/** Handle clicking on a clue: show the question or answer.
+ *
+ * Uses .showing property on clue to determine what to show:
+ * - if currently null, show question & set .showing to "question"
+ * - if currently "question", show answer & set .showing to "answer"
+ * - if currently "answer", ignore click
+ * */
+
+function handleClick(evt) {}
+
+/** Wipe the current Jeopardy board, show the loading spinner,
+ * and update the button used to fetch data.
+ */
+
+function showLoadingView() {}
+
+/** Remove the loading spinner and update the button used to fetch data. */
+
+function hideLoadingView() {}
+
+/** Start game:
+ *
+ * - get random category Ids
+ * - get data for each category
+ * - create HTML table
+ * */
+
+async function setupAndStart() {}
+
+/** On click of start / restart button, set up game. */
+
+// TODO
+
+/** On page load, add event handler for clicking clues */
+
+// TODO
